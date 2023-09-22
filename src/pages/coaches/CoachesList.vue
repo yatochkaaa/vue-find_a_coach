4a305e0d86c4b2245b1f8dfbdfb450d92c815dc4
Владимир Таранин <crm.c.30187@dz.com>
2023-09-22 16:04:46 +0300

    working on the coaches list and list items

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,7 +6,15 @@
       <router-link to="/register">Register as Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in coaches" :key="coach.id">{{ coach.firstName }}</li>
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      />
     </ul>
     <h3 v-else>No coaches found.</h3>
   </section>
@@ -14,15 +22,30 @@
 
 <script>
 import { mapGetters } from 'vuex';
+import CoachItem from '../../components/coaches/CoachItem.vue';
 
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     ...mapGetters({
-      coaches: 'coaches/coaches',
+      filteredCoaches: 'coaches/coaches',
       hasCoaches: 'coaches/hasCoaches',
     }),
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
