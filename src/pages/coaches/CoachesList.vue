8bf99e5d5ef2197c04e590bd83f177981dc7cf2f
Владимир Таранин <crm.c.30187@dz.com>
2023-09-25 19:51:26 +0300

    updating the ui based on auth state

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -12,7 +12,12 @@
           <base-button mode="outline" @click="loadCoaches(true)">
             Refresh
           </base-button>
-          <base-button v-if="!isCoach && !isLoading" link to="/register">
+          <base-button link to="/auth" v-if="!isLoggedIn">Login</base-button>
+          <base-button
+            v-if="isLoggedIn && !isCoach && !isLoading"
+            link
+            to="/register"
+          >
             Register as Coach
           </base-button>
         </div>
@@ -57,6 +62,9 @@ export default {
     };
   },
   computed: {
+    isLoggedIn() {
+      return this.$store.getters['auth/isAuthenticated'];
+    },
     coaches() {
       return this.$store.getters['coaches/coaches'];
     },
