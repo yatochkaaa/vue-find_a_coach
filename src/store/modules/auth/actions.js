0f3e73fc409dd57881c87035d6b00d278d7a8c36
Владимир Таранин <crm.c.30187@dz.com>
2023-09-26 13:25:14 +0300

    adding 'auto login'

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -2,46 +2,57 @@ import axios from 'axios';
 
 export default {
   async login(context, payload) {
-    const response = await axios.post(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBvDNoCmio0JQDRJjnY3Dm5gr5Q1Bwj2Uc',
-      {
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true,
-      }
-    );
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    });
+  },
+  async signup(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBvDNoCmio0JQDRJjnY3Dm5gr5Q1Bwj2Uc';
+
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBvDNoCmio0JQDRJjnY3Dm5gr5Q1Bwj2Uc';
+    }
+    const response = await axios.post(url, {
+      email: payload.email,
+      password: payload.password,
+      returnSecureToken: true,
+    });
 
     try {
       const responseData = response.data;
+
+      localStorage.setItem('token', responseData.idToken);
+      localStorage.setItem('userId', responseData.localId);
+
       context.commit('setUser', {
         token: responseData.idToken,
         userId: responseData.localId,
         tokenExpiration: responseData.expiresIn,
       });
-      console.log(responseData);
     } catch (error) {
       throw new Error(response.data.message);
     }
   },
-  async signup(context, payload) {
-    const response = await axios.post(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBvDNoCmio0JQDRJjnY3Dm5gr5Q1Bwj2Uc',
-      {
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true,
-      }
-    );
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
 
-    try {
-      const responseData = response.data;
+    if (token && userId) {
       context.commit('setUser', {
-        token: responseData.idToken,
-        userId: responseData.localId,
-        tokenExpiration: responseData.expiresIn,
+        token,
+        userId,
+        tokenExpiration: null,
       });
-    } catch (error) {
-      throw new Error(response.data.message);
     }
   },
   logout(context) {
